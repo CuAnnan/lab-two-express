74c58d488005184ec38f36cc1fa967271566caaa
Wing <eamonn.kearns.2024@mumail.ie>
2025-02-27 17:37:22 +0000

    Have the functionality done

--- public/index.js
+++ public/index.js
@@ -1,7 +1,28 @@
 // IIFE
 (function(){
+    // some html elements
     let $searchButton;
     let $temperatureFeedback;
+    let $pageFeedback;
+    let $pageFeedbackValue;
+    let $feedbackIcon;
+
+    // some control varialbe
+    let urlParts;
+    // this one will be the result of the constraints variable for the current page
+    let pageConstraints;
+
+    // an associative array of values that map the html page to the json field in the data to search for
+    // what value to use as the tipping point
+    // and what the below color and above color should be for those tipping points
+    const constraints = {
+        'temperature':  {field:'temperatureCelsius', boundSeparator:20,     below:'blue',    above:'yellow'},
+        'humidity':     {field:'humidity', boundSeparator:0.6, below:'lightblue', above:'gray'},
+        'uv':           {field:'uvIndex', boundSeparator:5, below:'orange', above:'yellow'},
+        'wind':         {field:'windSpeed', boundSeparator:19, below:'green', above:'orange'}
+    };
+
+    // helper functions
     function $(elementId)
     {
         return document.getElementById(elementId);
@@ -18,11 +39,16 @@
         return celsius * 9 / 5 + 32;
     }
 
+    /**
+     * Returns the page part of the link, ie the part before the .html, and any get params that have been passed to it in
+     * an associative array
+     * @returns {{page: string, args: {}}}
+     */
     function parseLocation()
     {
 
         let [url, argsString] =  document.location.href.split('?');
-        let keyValuePairs = argsString.split('&');
+        let keyValuePairs = argsString?argsString.split('&'):[];
         let args = {}
         for(let keyValuePair of keyValuePairs)
         {
@@ -35,23 +61,69 @@
         }
     }
 
+    function parseWeatherData(weatherData)
+    {
+        $pageFeedbackValue.innerHTML = weatherData;
+        if(parseInt(weatherData)>=pageConstraints.boundSeparator)
+        {
+            $feedbackIcon.style.color=pageConstraints.above;
+        }
+        else
+        {
+            $feedbackIcon.style.color=pageConstraints.below;
+        }
+    }
+
     window.addEventListener('load', function()
     {
-        let $pageFeedback = $(`feedback`);
-        let $pageFeedbackValue = $(`feedbackValue`);
-        let $feedbackIcon = $('feedbackIcon');
-        let urlParts = parseLocation();
-        console.log(urlParts);
-        $('citySearchButton').addEventListener('click', function(){
-            let city = $('city').value;
-            fetch('/getJSON?city='+city)
+        $pageFeedback = $(`feedback`);
+        $pageFeedbackValue = $(`feedbackValue`);
+        $feedbackIcon = $('feedbackIcon');
+        let $cityInput = $('city');
+
+        urlParts = parseLocation();
+        let city = urlParts?.args?.city;
+        if(city)
+        {
+            $cityInput.value = city;
+            $$('nav ul li a').forEach((link) => {
+                link.href = link.href.split('?')[0] + `?city=${city}`;
+            });
+        }
+
+
+
+        if(urlParts.page && urlParts.page !== "index")
+        {
+            pageConstraints =constraints[urlParts.page];
+            fetch(`/getWeatherJSON?city=${city}&field=${pageConstraints.field}`)
                 .then(res=>res.json())
                 .then(json=>{
-                    $$('nav ul li a').forEach((link)=>{
-                        link.href = link.href.split('?')[0]+`?city=${city}`;
-                    });
+                    parseWeatherData(json.data);
+                });
+        }
+
+        $('citySearchButton').addEventListener('click', function(){
+            let city = $cityInput.value;
+            $('JSONFeedback').innerHTML = "";
+            fetch('/getCityJSON?city='+city)
+                .then(res=>res.json())
+                .then(json=>{
+                    if(json.errorCode)
+                    {
+                        $('JSONFeedback').innerHTML = json.errorMessage;
+                    }
+                    else
+                    {
+                        $$('nav ul li a').forEach((link) => {
+                            link.href = link.href.split('?')[0] + `?city=${city}`;
+                        });
+                        if(urlParts.page && urlParts.page !== "index")
+                        {
+                            parseWeatherData(json.city[pageConstraints.field]);
+                        }
+                    }
                 });
         });
-
     });
 })();
--- routes/index.js
+++ routes/index.js
@@ -16,20 +16,40 @@ const jsonData = JSON.parse(data);
 
 
 /* GET home page. */
-router.get('/getJSON', function(req, res, next) {
+router.get('/getCityJSON', function(req, res, next) {
   let city = null;
   jsonData.forEach((cityJSON)=>{
     if(cityJSON.cityName.toLowerCase()===req.query.city.toLowerCase())
     {
       city = cityJSON;
+      city.city = req.query.city.toLowerCase();
     }
   });
   if(city)
   {
-    res.json(city);
+    res.json({city});
   }
-  else {
-    res.json({error: `No city ${req.query.city} found`});
+  else
+  {
+    res.json({errorCode:1, errorMessage: `No city ${req.query.city} found`});
+  }
+});
+
+router.get('/getWeatherJSON', function(req, res, next){
+  let data = null;
+  jsonData.forEach((cityJSON)=>{
+    if(cityJSON.cityName.toLowerCase()===req.query.city.toLowerCase())
+    {
+      data = cityJSON[req.query.field];
+    }
+  });
+  if(data)
+  {
+    res.json({data});
+  }
+  else
+  {
+    res.json({errorCode:1, errorMessage: `No city ${req.query.city} found`});
   }
 });
 
